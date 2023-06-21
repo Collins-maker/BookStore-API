4c7a3efd7a7bc5aa6af100fc5a70409b3815ec60
Collins Muchui <muchuicollins56@gmail.com>
2023-06-21 16:27:41 +0300

    destructured the schema to remove Users

--- src/database/basic.sql
+++ src/database/basic.sql
@@ -15,11 +15,7 @@ SELECT * FROM library.Members;
 --CREATE SCHEMA library;
 --GO 
 CREATE TABLE library.Books (
-<<<<<<< HEAD
 BookID INT PRIMARY KEY IDENTITY(1,1),
-=======
-BookID INT IDENTITY(1,1) PRIMARY KEY, 
->>>>>>> 6078b06beab3047918b67ce3261c800e27fdf16b
 Title VARCHAR (255) NOT NULL, 
 Author VARCHAR(255) NOT NULL,
 PublicationYear VARCHAR(255) NOT NULL,
@@ -65,17 +61,18 @@ SELECT * FROM library.Members;
 
 
 
-INSERT INTO library.Books (Title, Author, PublicationYear, Status) VALUES
-('To Kill a Mockingbird', 'Harper Lee', '1960', 'Available'),
-('1984', 'George Orwell', '1949', 'Available'),
-('The Great Gatsby', 'F. Scott Fitzgerald', '1925', 'Available'),
-('Pride and Prejudice', 'Jane Austen', '1813', 'Available'),
-('The Catcher in the Rye', 'J.D. Salinger', '1951', 'Available'),
-('The Hobbit', 'J.R.R. Tolkien', '1937', 'Available'),
-('To the Lighthouse', 'Virginia Woolf', '1927', 'Available'),
-('Brave New World', 'Aldous Huxley', '1932', 'Available'),
-('Moby-Dick', 'Herman Melville', '1851', 'Available'),
-('The Lord of the Rings', 'J.R.R. Tolkien', '1954', 'Available');
+INSERT INTO library.Members (Name, Address, ContactNumber, email, password) VALUES
+('John Smith', '123 Main St, Cityville', '555-1234', 'john.smith@example.com', 'secret001'),
+('Emily Johnson', '456 Elm St, Townville', '555-5678', 'emily.johnson@example.com', 'secret002'),
+('Michael Williams', '789 Oak St, Villagetown', '555-9012', 'michael.williams@example.com', 'secret003'),
+('Sophia Brown', '321 Pine St, Hamletville', '555-3456', 'sophia.brown@example.com', 'secret004'),
+('Daniel Taylor', '654 Maple St, Countryside', '555-7890', 'daniel.taylor@example.com', 'secret005'),
+('Olivia Martinez', '987 Cedar St, Hillside', '555-1234', 'olivia.martinez@example.com', 'secret006'),
+('James Davis', '741 Birch St, Riverside', '555-5678', 'james.davis@example.com', 'secret007'),
+('Emma Garcia', '852 Walnut St, Lakeside', '555-9012', 'emma.garcia@example.com', 'secret008'),
+('Benjamin Rodriguez', '369 Willow St, Mountainview', '555-3456', 'benjamin.rodriguez@example.com', 'secret009'),
+('Ava Wilson', '951 Cherry St, Beachside', '555-7890', 'ava.wilson@example.com', 'secret010');
+
 
 --To confirm if inserted execute:
 SELECT * FROM library.Books;
